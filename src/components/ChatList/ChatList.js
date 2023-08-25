ab67f0f19dccfd9d8ab1c53f7fdffc688fedc2d9
Shyam Natesan <shyamnatesan21@gmail.com>
2023-08-26 03:31:31 +0530

    UI => added scrollbar for chatlist and ChatWindow

--- src/components/ChatList/ChatList.js
+++ src/components/ChatList/ChatList.js
@@ -26,47 +26,48 @@ export default function ChatList({ handleToggleChatWindow }) {
 
   const handleSearch = () => {};
 
+  console.log(users);
+
   return (
-    <>
-      <div className="col-3 bg-light">
-        <h1
-          className="d-block p-3  text-decoration-none"
-          style={{ marginTop: "30px" }}
-        >
+    <div className="col-sm-3 bg-light sticky-top min-vh-100">
+      <div className="d-flex flex-sm-column flex-row flex-nowrap align-items-center sticky-top">
+        <h1 className="p-3  text-decoration-none" style={{ marginTop: "5%" }}>
           Messages
         </h1>
         <SearchBar handleSearch={handleSearch} />
-        <ul
-          className="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto"
-          style={{ marginTop: "50px" }}
-        >
-          <p>All Messages</p>
-          {users.map((user, index) => {
-            return (
-              <li
-                className="nav-item d-flex mb-3"
-                key={index}
-                onClick={() => handleUserClick(user)}
-              >
-                <img
-                  src={user.imageUrl}
-                  className="rounded-circle me-2"
-                  width="50px"
-                  height="50px"
-                ></img>
-                <a
-                  href="#"
-                  className="nav-link py-3 px-2"
-                  style={{ color: "black" }}
+
+        <p>All Messages</p>
+        <div className="container vh-100">
+          <div
+            className="overflow-auto p-3 bg-light"
+            style={{ width: "100%", height: "inherit" }}
+          >
+            {users.map((user, index) => {
+              return (
+                <li
+                  className="nav-item d-flex mb-3"
+                  key={index}
+                  onClick={() => handleUserClick(user)}
                 >
-                  {user.fullName}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+                  <img
+                    src={user.imageUrl}
+                    className="rounded-circle me-2"
+                    width="50px"
+                    height="50px"
+                  ></img>
+                  <a
+                    href="#"
+                    className="nav-link py-3 px-2"
+                    style={{ color: "black" }}
+                  >
+                    {user.fullName}
+                  </a>
+                </li>
+              );
+            })}
+          </div>
+        </div>
       </div>
-      {/* {selectedUser.length > 0 && <ChatWindow user={selectedUser} />} */}
-    </>
+    </div>
   );
 }
--- src/components/ChatWindow/ChatWindow.js
+++ src/components/ChatWindow/ChatWindow.js
@@ -57,43 +57,57 @@ export default function ChatWindow({ user, roomId }) {
   };
 
   return (
-    <div className="col-6 bg-light p-2 d-flex flex-column">
-      <div className="d-flex">
-        <img
-          src={user.imageUrl}
-          className="rounded-circle me-2"
-          width="50px"
-          height="50px"
-        ></img>
-        <a
-          href="#"
-          className="nav-link py-3 px-2 rounded"
-          style={{ color: "black" }}
-        >
-          {user.fullName}
-        </a>
-      </div>
-      {/* message display */}
-      <div className="flex-grow-1 p-2" style={componentStyle}>
-        {messages &&
-          messages.map((message) => {
-            return <p>{message.message}</p>;
-          })}
-      </div>
-      {/* message input */}
-      <div className="input-group mb-3 rounded">
-        <input
-          type="text"
-          className="form-control rounded-start"
-          placeholder="Type your message..."
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          style={{ borderColor: "#ced4da" }}
-        />
-        <button className="btn btn-primary rounded-end" onClick={sendMessage}>
-          <FontAwesomeIcon icon={faPaperPlane} />
-        </button>
+    <div className="col-sm-6 bg-light sticky-top min-vh-100">
+      <div className="d-flex flex-column p-3">
+        <div className="d-flex align-items-center mb-2">
+          <img
+            src={user.imageUrl}
+            className="rounded-circle me-2"
+            width="50px"
+            height="50px"
+          />
+          <a
+            href="#"
+            className="nav-link py-3 px-2 rounded"
+            style={{ color: "black" }}
+          >
+            {user.fullName}
+          </a>
+        </div>
+        <div className="container bg-dark rounded m-2">
+          <div
+            className="overflow-auto p-2"
+            style={{ width: "100%", height: "600px" }}
+          >
+            {messages.map((message, index) => (
+              <div className="d-block">
+                <span
+                  className={`p-2 mb-2 ${
+                    message.senderId === socket.id
+                      ? "mr-auto text-white"
+                      : "ml-auto text-white"
+                  }`}
+                >
+                  {message.message}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control rounded-start"
+            placeholder="Type your message..."
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            style={{ borderColor: "#ced4da" }}
+          />
+          <button className="btn btn-primary rounded-end" onClick={sendMessage}>
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div>
       </div>
     </div>
   );
